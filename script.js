9e74b1571544d0a0170f38e47627eeb30bbf0332
Tai-Ning <winnie1533@gmail.com>
2024-11-15 18:53:25 +0800

    Add loader

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let quotes = []
+const quoteContainer = document.getElementById('quote-container') 
 const quoteText = document.getElementById('quote')
 const quoteAuthor = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
-const newQuoteBtn =document.getElementById('new-quote')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+//Show loader
+function loading(){
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+//Hide loader
+function complete(){
+    loader.hidden = true
+    quoteContainer.hidden = false
+}
 
 //Get new quote
 function randomQuote() {
+    loading()
     const quote = quotes[Math.floor(Math.random()*quotes.length)]
     //Check quote length to determine styling
     if(quote.text.length>120){
@@ -20,14 +34,17 @@ function randomQuote() {
     }else{
         quoteAuthor.textContent = quote.author
     }
+    complete()
 }
 
 //Get quotes from API
 async function getQuotes() {
-    try{
+    loading()
     const api = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
+    try{
     const response = await fetch(api)
     quotes= await response.json()
+    randomQuote()
     }catch(error){
         console.error('Fail to catch quotes from API')
     }
